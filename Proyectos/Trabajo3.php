df1196cccfe6bc8bee452a650dbcc12025930d92
JhanCarlos-117 <bastidasmartinezj@gmail.com>
2020-04-25 15:23:52 -0500

    Estilos agregados al trabajo3

--- Proyectos/Trabajo3.php
+++ Proyectos/Trabajo3.php
@@ -2,18 +2,20 @@
 <html lang="es" dir="ltr">
   <head>
     <meta charset="utf-8">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="styles.css">
     <title>ACTIVIDAD 3</title>
   </head>
   <body>
-      <FORM ACTION = "pagina21.php" METHOD = "post">s
-      Nombre:
-      <input type="text" name="nombre"><br>
-      Peso en Kg:
-      <input type="text" name="peso"><br>
-      Altura en M:
-      <input type="text" name="altura"><br>
+      <FORM ACTION = "pagina21.php" METHOD = "post">
+      <p>Nombre:</p>
+      <input class="form-inline" type="text" name="nombre"><br>
+      <p>Peso en Kg:</p>
+      <input class="form-inline" type="text" name="peso"><br>
+      <p>Altura en Metros:</p>
+      <input class="form-inline" type="text" name="altura"><br>
 
-      <input type="submit" value="CALCULAR">
+      <input class="btn-primary" type="submit" value="CALCULAR">
     </FORM>
 
   </body>
