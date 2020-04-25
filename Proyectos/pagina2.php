e7f73c51e72d53efe4cfbc4ebc2d348431ee075e
JhanCarlos-117 <bastidasmartinezj@gmail.com>
2020-04-25 13:29:55 -0500

    Estilos agregados al trabajo1

--- Proyectos/pagina2.php
+++ Proyectos/pagina2.php
@@ -2,9 +2,11 @@
 <html lang="es" dir="ltr">
   <head>
     <meta charset="utf-8">
+    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
     <title>PAGINA 2</title>
   </head>
-  <body>
+  <body class="php1">
     <?php
      $nota1 = $_POST ['nota1'];
      $nota2 = $_POST ['nota2'];
@@ -14,6 +16,20 @@
 
      $resultado =   ($nota1 + $nota2 + $nota3)*35/100 + ($examen)*35/100 + ($trabajo)* 30/100;
      ?>
-     Definitiva:  <?php echo ($resultado)/5 ?>
+     <table class="table" border="2">
+       <thead class="thead-dark">
+         <tr>
+           <th><h1>Definitiva:</h1></th>
+         </tr>
+       </thead>
+       <tbody class="thead-light">
+         <tr>
+           <th>
+             <?php echo ($resultado)/5 ?>
+          </th>
+         </tr>
+       </tbody>
+     </table>
+     <a  href="trabajo1.php"> <input class="btn-primary" type="submit" value="Regresar"></a>
   </body>
 </html>
--- Proyectos/trabajo1.php
+++ Proyectos/trabajo1.php
@@ -2,22 +2,24 @@
 <html lang="es" dir="ltr">
   <head>
     <meta charset="utf-8">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="styles.css">
     <title>EJERCICIO 1</title>
   </head>
   <body>
   <FORM ACTION = "pagina2.php" METHOD = "post">
-    Nota1:
-    <input type="text" name="nota1"><br>
-    Nota2:
-    <input type="text" name="nota2"><br>
-    Nota3:
-    <input type="text" name="nota3"><br>
-    Examen:
-    <input type="text" name="examen"><br>
-    Trabajo:
-    <input type="text" name="trabajo"><br>
-    
-    <input type="submit" value="Enviar">
+    <p>Nota1:</p>
+    <input class="form-inline" type="text" name="nota1"><br>
+    <p>Nota2:</p>
+    <input class="form-inline" type="text" name="nota2"><br>
+    <p>Nota3:</p>
+    <input class="form-inline" type="text" name="nota3"><br>
+    <p>Examen:</p>
+    <input class="form-inline" type="text" name="examen"><br>
+    <p>Trabajo:</p>
+    <input class="form-inline" type="text" name="trabajo"><br>
+
+    <input class="btn-primary" type="submit" value="Enviar">
   </FORM>
   </body>
 </html>
