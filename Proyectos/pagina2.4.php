84b632f8ddb375d056326676772e31330d62ab0d
JhanCarlos-117 <bastidasmartinezj@gmail.com>
2020-04-26 13:07:47 -0500

    Agregados estilos trabajo 5

--- Proyectos/pagina2.4.php
+++ Proyectos/pagina2.4.php
@@ -2,26 +2,51 @@
 <html lang="es" dir="ltr">
   <head>
     <meta charset="utf-8">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="styles.css">
     <title>ARTICULO</title>
   </head>
   <body>
     <?php
-      echo $_POST['articulo']."<br>";
       $clave = $_POST['clave'];
       $precio = $_POST['precio'];
-      echo $clave."<br>";
-      echo $precio."<br>";
-
-      if ($clave == "1") {
-        $clavej =$precio-($precio)*10/100;
-              echo $clavej;
-      }
-      else {
-      if ($clave == "2") {
-          $r =$precio-($precio)*20/100;
-          echo $r;
-      }
-        }
      ?>
   </body>
+  <table class="table">
+    <thead class="table-dark">
+      <td>Articulo</td>
+    </thead>
+    <tr>
+      <th><?php echo $_POST['articulo']."<br>"; ?></th>
+    </tr>
+    <thead class="table-dark">
+      <td>Clave</td>
+    </thead>
+    <tr>
+      <th><?php echo $clave."<br>";?></th>
+    </tr>
+    <thead class="table-dark">
+      <td>Precio Del Articulo</td>
+    </thead>
+    <tr>
+      <th><?php echo $precio."<br>"; ?></th>
+    </tr>
+    <thead class="table-dark">
+      <td>Descuento</td>
+    </thead>
+    <tr>
+      <th><?php   if ($clave == "1") {
+          $clavej =$precio-($precio)*10/100;
+                echo $clavej;
+        }
+        else {
+        if ($clave == "2") {
+            $r =$precio-($precio)*20/100;
+            echo $r;
+        }
+          }
+          ?></th>
+    </tr>
+  </table>
+  <a class="btn1" href="trabajo5.php"><input class="btn-primary" type="submit" value="Regresar"></a>
 </html>
