63828fdc726ba8531c756d832f756811fadd824e
JhanCarlos-117 <bastidasmartinezj@gmail.com>
2020-04-25 23:38:53 -0500

    estilos trabajo4

--- Proyectos/pagina2.3.php
+++ Proyectos/pagina2.3.php
@@ -2,6 +2,8 @@
 <html lang="es" dir="ltr">
   <head>
     <meta charset="utf-8">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="styles.css">
     <title>CANTIDAD</title>
   </head>
   <body>
@@ -10,22 +12,36 @@
         $ginecologia = $_POST['cantidad'];
         $traumatologia = $_POST['cantidad'];
         $pediatria = $_POST['cantidad'];
-        $hospita = "Presupuesto del Hospital:";
-        $ginecologia2 = "Presupuesto de Ginecologia:";
-        $traumatologia2 = "Presupuesto de Traumatologia:";
-        $pediatria2 = "Presupuesto de Pediatria Pediatria:";
 
         $ginecologia1 = ($ginecologia)*40/100;
         $traumatologia1 = ($traumatologia)*30/100;
         $pediatria1 = ($pediatria)*30/100;
-        echo $hospita;
-        echo $hospital."<br>";
-        echo $ginecologia2;
-        echo $ginecologia1."<br>";
-        echo $traumatologia2;
-        echo $traumatologia1."<br>";
-        echo $pediatria2;
-        echo $pediatria1."<br>";
      ?>
+     <table class="table">
+       <thead class="table-dark">
+         <td>Presupuesto del Hospital:</td>
+       </thead>
+       <tr>
+         <th><?php echo $hospital."<br>";  ?></th>
+       </tr>
+       <thead class="table-dark">
+         <td>Presupuesto de Ginecologia:</td>
+       </thead>
+       <tr>
+         <th><?php   echo $ginecologia1."<br>"; ?></th>
+       </tr>
+       <thead class="table-dark">
+         <td>Presupuesto de Traumatologia:</td>
+       </thead>
+       <tr>
+         <th><?php   echo $traumatologia1."<br>";  ?></th>
+       </tr>
+       <thead class="table-dark">
+         <td>Presupuesto de Pediatria Pediatria:</td>
+       </thead>
+       <tr>
+         <th><?php echo $pediatria1."<br>";  ?></th>
+       </tr>
+     </table>
   </body>
 </html>
--- Proyectos/trabajo4.php
+++ Proyectos/trabajo4.php
@@ -2,12 +2,14 @@
 <html lang="es" dir="ltr">
   <head>
     <meta charset="utf-8">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="styles.css">
     <title>ACTIVIDAD 4</title>
   </head>
   <body>
     <FORM ACTION ="pagina2.3.php" METHOD = "post">
-      Presupuesto del Hospital:<input type="text" name="cantidad"><br>
-      <input type="submit" value="Enviar">
+    <p>Presupuesto del Hospital:</p> <input type="text" name="cantidad"><br>
+  <br> <input class="btn-primary" type="submit" value="Enviar">
     </FORM>
   </body>
 </html>
